ab8559194bcf1f64ddcd2747d7d52dc83b6004e7
MohammadOwais786 <mohammadowaiz.786@gmail.com>
2016-11-25 11:51:42 +0530

    CassandraConnector Updated

--- BasicCassandraApp/src/main/java/com/shaik/cassandra/BasicCassandraApp/CassandraConnect.java
+++ BasicCassandraApp/src/main/java/com/shaik/cassandra/BasicCassandraApp/CassandraConnect.java
@@ -1,25 +1,58 @@
 package com.shaik.cassandra.BasicCassandraApp;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 
 public class CassandraConnect {
 
-	private Cluster cluster;
-	 public Session session;
+    private Cluster cluster;
+    public Session session;
 
-	public void connect() {
-		this.cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-		session = cluster.connect("shaik");
-	}
+    public void connect() {
+        this.cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+        session = cluster.connect("shaik");
+    }
 
-	public Session getSession() {
-		return this.session;
-	}
+    public Session getSession() {
+        return this.session;
+    }
 
-	/** Close cluster. */
-	public void close() {
-		cluster.close();
-	}
+    public void adder(String a) {
+        getSession().execute("INSERT INTO users(id,name,address) VALUES" + "(" + a + ")");
+        System.out.println("Successfully Added !");
+
+    }
+
+    public void updater(int id, String v) {
+
+        Statement Query = QueryBuilder.update("users").with(QueryBuilder.set("address", v))
+                .where(QueryBuilder.eq("id", id));
+        session.execute(Query);
+        System.out.println("Successfully Updated !");
+
+    }
+
+    public void deleter(int id) {
+
+        Statement Query = QueryBuilder.delete().from("users").where(QueryBuilder.eq("id", id));
+        session.execute(Query);
+        System.out.println("Successfully Deleted !");
+    }
+
+    public void ViewTable() {
+        ResultSet result = getSession().execute("Select * from users");
+        for (Row row : result) {
+            System.out.format("%d - %s - %s\n", row.getInt("id"), row.getString("name"), row.getString("address"));
+        }
+    }
+
+    /** Close cluster. */
+    public void close() {
+        cluster.close();
+    }
 
 }
--- BasicCassandraApp/src/main/java/com/shaik/cassandra/BasicCassandraApp/EnIDAOImpl.java
+++ BasicCassandraApp/src/main/java/com/shaik/cassandra/BasicCassandraApp/EnIDAOImpl.java
@@ -1,59 +1,37 @@
 package com.shaik.cassandra.BasicCassandraApp;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.Statement;
-import com.datastax.driver.core.querybuilder.QueryBuilder;
-
 public class EnIDAOImpl implements EnIDAO {
 
-    private Cluster cluster;
-    private Session session;
+    private CassandraConnect cassandra;
 
     public EnIDAOImpl() {
 
-        this.cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-        session = cluster.connect("shaik");
+        cassandra = new CassandraConnect();
+        cassandra.connect();
 
     }
 
     public void add(String a) {
-        getSession().execute("INSERT INTO users(id,name,address) VALUES" + "(" + a + ")");
-        System.out.println("Successfully Added !");
 
+        cassandra.adder(a);
     }
 
     public void update(int id, String v) {
 
-        Statement Query = QueryBuilder.update("users").with(QueryBuilder.set("address", v))
-                .where(QueryBuilder.eq("id", id));
-        session.execute(Query);
-        System.out.println("Successfully Updated !");
+        cassandra.updater(id, v);
 
     }
 
     public void delete(int id) {
-
-        Statement Query = QueryBuilder.delete().from("users").where(QueryBuilder.eq("id", id));
-        session.execute(Query);
-        System.out.println("Successfully Deleted !");
+        cassandra.deleter(id);
     }
 
     public void view() {
-        ResultSet result = getSession().execute("Select * from users");
-        for (Row row : result) {
-            System.out.format("%d - %s - %s\n", row.getInt("id"), row.getString("name"), row.getString("address"));
-        }
-    }
-
-    public Session getSession() {
-        return this.session;
+        cassandra.ViewTable();
     }
 
     public void close() {
-        cluster.close();
+        cassandra.close();
     }
 
 }
